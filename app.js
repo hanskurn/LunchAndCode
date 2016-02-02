8efd65802939c3f41af5b2253343743592c28fe2
Hannah Skurnik <hskurnik@deloitte.ca>
2016-02-02 08:36:14 -0800

    initial commit

---
+++ app.js
@@ -0,0 +1,27 @@
+(function(){
+	angular.module('Appx')
+		.controller('ListController',ListController);
+
+
+		function ListController(){
+
+			var vm = this;
+
+			this.list = [];
+
+			this.click = click;
+			
+
+
+			function click(){
+
+
+			}
+
+			function calculate(){
+
+			}
+
+		}
+
+	})();
