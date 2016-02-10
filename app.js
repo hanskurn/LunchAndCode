1f0e23a3b2eb1ea72a127df461dfe9ac930e2b89
Hannah Skurnik <hskurnik@deloitte.ca>
2016-02-09 16:15:26 -0800

    added in app logic

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 (function(){
-	angular.module('Appx')
+	angular.module('GymGoals', [])
 		.controller('ListController',ListController);
 
 
@@ -7,18 +7,36 @@
 
 			var vm = this;
 
-			this.list = [];
+			this.newGoal;
+			this.start;
+			this.goalEnd;
 
-			this.click = click;
+			this.goals = [
+				{goal:'Pushups',start:10, endGoal:30 },
+				{goal:'Situps',start:30, endGoal:100 },
+				{goal:'Track laps',start:10, endGoal:15 },
+			];
+
+			this.addGoal = addGoal;
+			this.calculatePercentageChange = calculatePercentageChange
 			
 
+			function addGoal(){
+				var newGoal = {};
+				newGoal.goal = this.newGoal;
+				newGoal.start = this.start;
+				newGoal.endGoal = this.goalEnd;
+				this.goals.push(newGoal);
 
-			function click(){
-
+				this.newGoal = null;
+				this.start = null;
+				this.goalEnd = null;
 
 			}
 
-			function calculate(){
+			function calculatePercentageChange(start, end){
+				var change = ((end-start) /start) *100;
+				return change;
 
 			}
 
